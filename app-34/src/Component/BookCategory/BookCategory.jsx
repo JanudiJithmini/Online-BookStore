05014f8beb2fe7a772891829291809c80c03e2ff
Janudi <akmeemanajanudi@gamil.com>
2025-03-16 23:52:51 +0530

    Adding changes into the Category-page branch

--- app-34/src/Component/BookCategory/BookCategory.jsx
+++ app-34/src/Component/BookCategory/BookCategory.jsx
@@ -1,7 +1,18 @@
 import React from "react";
 import "./BookCategory.css";
 function BookCategory() {
-  return <div className="booklist-container">Welcome to BookCategory</div>;
+  return (
+    <div className="Category-page">
+      <div className="category-dropdown">
+        <label>Categories: </label>
+        <select>
+          <option value="fiction">Fiction</option>
+          <option value="childrens-story">Children's Story</option>
+          <option value="novels">Novels</option>
+        </select>
+      </div>
+    </div>
+  );
 }
 
 export default BookCategory;
